ceada3563558212b407b0536223b921f9b5c7513
Kirill_Kochugin <kraingor@yandex.ru>
2023-08-03 17:02:27 +0300

    Hello world

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello world!")
+
+
+def fake_bin(x):
+    result = ''
+    for i in x:
+        if i in '234':
+            result += '0'
+        elif i in '56789':
+            result += '1'
+        else:
+            result += i
+    return result
+    pass
+
+number = '3214323242111100000'
+
+output = fake_bin(number)
+
+print(output)
+
+def fake_bin(x):
+    result = ''
+    for i in x:
+        if i in '01234':
+            result += '0'
+        elif i in '56789':
+            result += '1'
+        else:
+            result += i
+    return result
+    pass
