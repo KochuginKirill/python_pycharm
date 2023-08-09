968fad0f1af9398fd8c0a26da156c32674e0d43b
Kirill_Kochugin <kraingor@yandex.ru>
2023-08-09 15:40:37 +0300

    server corrected

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello world'
+
+
+@app.route('/name/<name>/id/<number>/')
+def func_name(name, number):
+    number_user = int(number) * 100
+    html = '''
+
+    '''
+    return f'<h1>Привет</h1>, <p><b>{name}</b> - <i>{number_user}</i></p>'
 
 
 if __name__ == '__main__':
