6ba6285daa976df7e39c4fdea36007b47a3539b5
Kirill_Kochugin <kraingor@yandex.ru>
2023-09-13 13:36:14 +0300

    dz_7_added

--- DZ_Seminar_6/HARD2.py
+++ DZ_Seminar_6/HARD2.py
@@ -50,100 +50,102 @@ def try_int_get(number):
 
 
 def bot_xo(field, turn):
-        if [a[0] for a in field].count('|_|') == 0 and [a[1] for a in field].count('|_|') == 0 and [a[2] for a in field].count('|_|') == 0:
+    if [a[0] for a in field].count('|_|') == 0 and [a[1] for a in field].count('|_|') == 0 and [a[2] for a in
+                                                                                                field].count(
+            '|_|') == 0:
+        string_1 = ''.join(map(str, field[0][:]))
+        string_2 = ''.join(map(str, field[1][:]))
+        string_3 = ''.join(map(str, field[2][:]))
+        msgbox(f"""{string_1}
+{string_2}
+{string_3}""", 'поле')
+        print('Ничья')
+        msgbox('Ничья')
+        exit()
+    else:
+        if turn:
             string_1 = ''.join(map(str, field[0][:]))
             string_2 = ''.join(map(str, field[1][:]))
             string_3 = ''.join(map(str, field[2][:]))
-            msgbox(f"""{string_1}
+            string = enterbox(f"""{string_1}
 {string_2}
-{string_3}""", 'поле')
-            print('Ничья')
-            msgbox('Ничья')
-            exit()
-        else:
-            if turn:
+{string_3}""", 'Введите номер строки, где будет Х', '')
+            string = try_int_get(string)
+            column = enterbox(f"""{string_1}
+{string_2}
+{string_3}""", 'Введите номер столбца, где будет Х', '')
+            column = try_int_get(column)
+            if field[string][column] == '|_|':
+                field[string][column] = '|X|'
                 string_1 = ''.join(map(str, field[0][:]))
                 string_2 = ''.join(map(str, field[1][:]))
                 string_3 = ''.join(map(str, field[2][:]))
-                string = enterbox(f"""{string_1}
-{string_2}
-{string_3}""", 'Введите номер строки, где будет Х', '')
-                string = try_int_get(string)
-                column = enterbox(f"""{string_1}
-{string_2}
-{string_3}""", 'Введите номер столбца, где будет Х', '')
-                column = try_int_get(column)
-                if field[string][column] == '|_|':
-                    field[string][column] = '|X|'
-                    string_1 = ''.join(map(str, field[0][:]))
-                    string_2 = ''.join(map(str, field[1][:]))
-                    string_3 = ''.join(map(str, field[2][:]))
-                    msgbox(f"""{string_1}
+                msgbox(f"""{string_1}
 {string_2}
 {string_3}""", 'ход бота')
-                    if field[:][0].count('|X|') == 3 or field[:][1].count('|X|') == 3 or field[:][2].count('|X|') == 3:
-                        msgbox('Вы победили!')
-                        exit()
-                    if [a[0] for a in field].count('|X|') == 3 or [a[1] for a in field].count('|X|') == 3 or [a[2] for a
-                                                                                                              in
-                                                                                                              field].count(
-                        '|X|') == 3:
-                        msgbox('Вы победили!')
-                        exit()
-                    if field[0][0] == '|X|' and field[1][1] == '|X|' and field[2][2] == '|X|':
-                        msgbox('Вы победили!')
-                        exit()
-                    if field[0][2] == '|X|' and field[1][1] == '|X|' and field[2][0] == '|X|':
-                        msgbox('Вы победили!')
-                        exit()
-                    turn = False
-                    return bot_xo(field, turn)
-                else:
-                    msgbox('Нарушать правила не хорошо, бот покидает вас с разочарованием')
+                if field[:][0].count('|X|') == 3 or field[:][1].count('|X|') == 3 or field[:][2].count('|X|') == 3:
+                    msgbox('Вы победили!')
                     exit()
+                if [a[0] for a in field].count('|X|') == 3 or [a[1] for a in field].count('|X|') == 3 or [a[2] for a
+                                                                                                          in
+                                                                                                          field].count(
+                    '|X|') == 3:
+                    msgbox('Вы победили!')
+                    exit()
+                if field[0][0] == '|X|' and field[1][1] == '|X|' and field[2][2] == '|X|':
+                    msgbox('Вы победили!')
+                    exit()
+                if field[0][2] == '|X|' and field[1][1] == '|X|' and field[2][0] == '|X|':
+                    msgbox('Вы победили!')
+                    exit()
+                turn = False
+                return bot_xo(field, turn)
             else:
-                if field[1][1] != '|X|' and field[1][1] != '|0|':
-                    field[1][1] = '|0|'
-                    print('первый ход бота: ')
-                    string_1 = ''.join(map(str, field[0][:]))
-                    string_2 = ''.join(map(str, field[1][:]))
-                    string_3 = ''.join(map(str, field[2][:]))
-                    msgbox(f"""{string_1}
+                msgbox('Нарушать правила не хорошо, бот покидает вас с разочарованием')
+                exit()
+        else:
+            if field[1][1] != '|X|' and field[1][1] != '|0|':
+                field[1][1] = '|0|'
+                print('первый ход бота: ')
+                string_1 = ''.join(map(str, field[0][:]))
+                string_2 = ''.join(map(str, field[1][:]))
+                string_3 = ''.join(map(str, field[2][:]))
+                msgbox(f"""{string_1}
 {string_2}
 {string_3}""", 'следующий ход')
-                    turn = True
-                    return bot_xo(field, turn)
-                else:
-                    try_turn = 'check'
-                    while try_turn != '|_|':
-                        random_string = random.randint(0, 2)
-                        random_column = random.randint(0, 2)
-                        try_turn = field[random_string][random_column]
-                        if try_turn == '|_|':
-                            field[random_string][random_column] = '|0|'
-                            print('ход бота: ')
-                            string_1 = ''.join(map(str, field[0][:]))
-                            string_2 = ''.join(map(str, field[1][:]))
-                            string_3 = ''.join(map(str, field[2][:]))
-                            msgbox(f"""{string_1}
+                turn = True
+                return bot_xo(field, turn)
+            else:
+                try_turn = 'check'
+                while try_turn != '|_|':
+                    random_string = random.randint(0, 2)
+                    random_column = random.randint(0, 2)
+                    try_turn = field[random_string][random_column]
+                    if try_turn == '|_|':
+                        field[random_string][random_column] = '|0|'
+                        print('ход бота: ')
+                        string_1 = ''.join(map(str, field[0][:]))
+                        string_2 = ''.join(map(str, field[1][:]))
+                        string_3 = ''.join(map(str, field[2][:]))
+                        msgbox(f"""{string_1}
 {string_2}
 {string_3}""", 'теперь ход бота')
-                            if field[:][0].count('|0|') == 3 or field[:][1].count('|0|') == 3 or field[:][2].count(
-                                    '|0|') == 3:
-                                msgbox('Бот победил!')
-                                exit()
-                            if [a[0] for a in field].count('|0|') == 3 or [a[1] for a in field].count('|0|') == 3 or [
-                                a[2] for a in field].count('|0|') == 3:
-                                msgbox('Бот победил!')
-                                exit()
-                            if field[0][0] == '|0|' and field[1][1] == '|0|' and field[2][2] == '|0|':
-                                msgbox('Бот победил!')
-                                exit()
-                            if field[0][2] == '|0|' and field[1][1] == '|0|' and field[2][0] == '|0|':
-                                msgbox('Бот победил!')
-                                exit()
-                            turn = True
-                            return bot_xo(field, turn)
+                        if field[:][0].count('|0|') == 3 or field[:][1].count('|0|') == 3 or field[:][2].count(
+                                '|0|') == 3:
+                            msgbox('Бот победил!')
+                            exit()
+                        if [a[0] for a in field].count('|0|') == 3 or [a[1] for a in field].count('|0|') == 3 or [
+                            a[2] for a in field].count('|0|') == 3:
+                            msgbox('Бот победил!')
+                            exit()
+                        if field[0][0] == '|0|' and field[1][1] == '|0|' and field[2][2] == '|0|':
+                            msgbox('Бот победил!')
+                            exit()
+                        if field[0][2] == '|0|' and field[1][1] == '|0|' and field[2][0] == '|0|':
+                            msgbox('Бот победил!')
+                            exit()
+                        turn = True
+                        return bot_xo(field, turn)
 
 
 field_generated = field_generator('Ваше поле в окне:')
--- DZ_Seminar_8/Telephone_database.py
+++ DZ_Seminar_8/Telephone_database.py
@@ -1,26 +1,116 @@
-phonebook = {'Дядя Ваня': {'phones': ['+78002553535', '+79772562534'], 'BD': "12.11.1973", 'eMail': "gggg@yandex.ru"}}
-print (phonebook['Дядя Ваня']['phones'][0])
+from easygui import *
+import re
+import os
 
-sp = {'дядя коля': {'номер телефона': ['8-800-555-35-35', '8-900-536-35-35'], 'город': 'Сыктыфкар', 'Статус': 'дядя'},
-      'нюрка': {'номер телефона': ['8-555-549-34-35'], 'город': 'Сыктыфкар', 'Статус': 'тетя'}}
+
+def try_int_get(number):
+    try:
+        number = int(number)
+    except ValueError:
+        msgbox('Вы ввели не число')
+        exit()
+    return int(number)
+
+
+def show_phonebook():
+    print('==========================================================')
+    data = open('BD_phonebook.txt', 'r', encoding = "UTF-8")
+    temp = sorted(data.readlines())
+    counter = 1
+    for i in range(len(temp)):
+        if len(temp[i]) > 0:
+            print(f'{counter}. {temp[i]}')
+            counter += 1
+    data.close()
+
+
+def add_line_phonebook():
+    data = open('BD_phonebook.txt', 'a', encoding = "UTF-8")
+    new_name = enterbox('Введите название нового контакта')
+    new_number = enterbox('Введите норер(а) нового контакта')
+    data.writelines(f'''
+{new_name} {new_number}''')
+    data.close()
+
+
+def search_line_phonebook():
+    data = open('BD_phonebook.txt', 'r', encoding = "UTF-8")
+    search = enterbox('Введите имя или телефон того, кого хотите найти')
+    result = ''
+    for line in data:
+        if search.lower() in line.lower():
+            result = line
+    if len(result) > 0:
+        msgbox(f'Нашелся: {result}')
+    else:
+        msgbox('Такой контакт не найден')
+        data.close()
+
+
+def change_line_phonebook():
+    data = open('BD_phonebook.txt', 'r', encoding = "UTF-8")
+    search = enterbox('Введите имя или телефон того, кого хотите изменить')
+    result = ''
+    for line in data:
+        if search.lower() in line.lower():
+            result = line
+    if result is not None:
+        msgbox(f'Найден следующий элемент {result}')
+    else:
+        msgbox('Элемент для удаления не найден')
+    data.close()
+    new_name = enterbox('Введите новое имя')
+    new_telephone = enterbox('Введите новый телефон(ы)')
+    input_result = new_name + ' ' + new_telephone
+    with open('BD_phonebook.txt', 'r', encoding = "UTF-8") as dana_inputed:
+        old_data = dana_inputed.read()
+    new_data = old_data.replace(f'{result}', f'{input_result}')
+    with open('BD_phonebook.txt', 'w', encoding = "UTF-8") as dana_inputed:
+        dana_inputed.write(new_data)
+
+
+def delete_line_phonebook():
+    data = open('BD_phonebook.txt', 'r', encoding = "UTF-8")
+    search = enterbox('Введите имя или телефон того, кого хотите удалить')
+    result = ''
+    for line in data:
+        if search.lower() in line.lower():
+            result = line
+    if result is not None:
+        msgbox(f'Найден следующий элемент {result}')
+    else:
+        msgbox('Элемент для удаления не найден')
+    data.close()
+    with open('BD_phonebook.txt', encoding = "UTF-8") as BD_phonebook:
+        lines = BD_phonebook.readlines()
+    pattern = re.compile(re.escape(result))
+    with open('BD_phonebook.txt', 'w', encoding = "UTF-8") as BD_phonebook:
+        for line in lines:
+            result = pattern.search(line)
+            if result is None:
+                BD_phonebook.write(line)
+    BD_phonebook.close()
+    msgbox('Элемент удален')
+
+
 
 while True:
-    command = input('Введите команду: ')
-    if command == '/all':
-        print('Вот текущий список номеров')
-        print(sp)
-    elif command == '/add.contact':
-        f = input('Введите имя нового контакта: ')
-        if f in sp:
-            print('Контакт существует!')
-        else:
-            coll = int(input('Сколько номеров вы хотите ввести: '))
-            i = 0
-            while coll > 0:
-                n = input('Введите первый номер: ')
-                sp[f]['номер телефона'][i] = n
-                i += 1
-                coll -= 1
-        print('Номер добавлен в базу данных')
-    elif command == '/help':
-        print('Здесь какойто мануал')
+    user_input = enterbox('''1 - Напечатать телефонную книгу в терминале
+2 - Добавить контакт в  телефонной книге
+3 - Найти контакт в телефонной книге
+4 - Изменить контакт в телефонной книге
+5 - Удалить контакт в телефонной книге
+6 - выход''', 'Введите цифру запроса')
+    user_input = try_int_get(user_input)
+    if user_input == 1:
+        show_phonebook()
+    elif user_input == 2:
+        add_line_phonebook()
+    elif user_input == 3:
+        search_line_phonebook()
+    elif user_input == 4:
+        change_line_phonebook()
+    elif user_input == 5:
+        delete_line_phonebook()
+    elif user_input == 6:
+          break
